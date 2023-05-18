602b9fecbbb2fa8c7c4121f5218181792ebf1b4f
elena-fly17 <elena17mr@gmail.com>
2023-05-18 14:43:41 +0300

    Создан микросервис client-updater

---
+++ src/main/java/com/example/clientupdater/ClientUpdaterApplication.java
@@ -0,0 +1,13 @@
+package com.example.clientupdater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientUpdaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientUpdaterApplication.class, args);
+    }
+
+}
